93ad272e60bbe8582745a06c80c9cf89b54d7bbc
SunnyTany <sunnytanyusha@gmail.com>
2021-03-24 14:26:37 +0200

    main

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.top__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        fade: true,
+        arrows: false
+    });
+});
